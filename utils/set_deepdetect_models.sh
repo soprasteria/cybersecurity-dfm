7c47ca31bbc74a654e1ab858b1a1a538ca03bea6
acabrol <alexandre.cabrol@soprasteria.com>
2017-06-08 03:10:03 +0200

    fix supervisor config file, add deepdetect models setter in utils

---
+++ utils/set_deepdetect_models.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd ../
+full_path=`pwd`
+for dir in `find $full_path/models -type d`
+do
+    dir=${dir%*/}
+    nb_classes=`wc -l $dir/corresp.txt|sed 's/\([^ ]\+\) .*/\1/g'`
+    echo ${dir##*/}
+    echo nb_classes
+    curl -X DELETE "http://localhost:8080/services/$dir"
+    curl -X PUT "http://localhost:8080/services/$dir" -d "{\"mllib\":\"xgboost\",\"description\":\"$dir\",\"type\":\"supervised\",\"parameters\":{\"input\":{\"connector\":\"txt\"},\"mllib\":{\"nclasses\":$nb_classes}},\"model\":{\"repository\":\"$full_path/$dir\"}}"
+
+done
