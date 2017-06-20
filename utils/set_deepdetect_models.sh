a9edf296b52a9e2292e45db3f2f29ba6276dfb93
Thibault Maurel <thibault.maurel@soprasteria.com>
2017-06-20 17:37:31 +0200

    DeepDetect settings script

--- utils/set_deepdetect_models.sh
+++ utils/set_deepdetect_models.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 cd ../
 full_path=`pwd`
-for dir in `find $full_path/models -type d`
+for dir in `find models -type d`
 do
     dir=${dir%*/}
     nb_classes=`wc -l $dir/corresp.txt|sed 's/\([^ ]\+\) .*/\1/g'`
-    echo ${dir##*/}
-    echo nb_classes
+    echo ${full_path}/${dir##*/}
+    echo $nb_classes
     curl -X DELETE "http://localhost:8080/services/$dir"
     curl -X PUT "http://localhost:8080/services/$dir" -d "{\"mllib\":\"xgboost\",\"description\":\"$dir\",\"type\":\"supervised\",\"parameters\":{\"input\":{\"connector\":\"txt\"},\"mllib\":{\"nclasses\":$nb_classes}},\"model\":{\"repository\":\"$full_path/$dir\"}}"
 
