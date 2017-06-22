bf889949015cfbc8959c9dc47e8bd72b0bc81fda
Thibault Maurel <thibault.maurel@soprasteria.com>
2017-06-22 14:09:52 +0200

    Docker container's ports are exposed

--- run.sh
+++ run.sh
@@ -3,4 +3,9 @@
 DFM_PATH=`pwd`
 
 docker build -t dfm .
-docker run --privileged=true --cap-add=SYS_ADMIN -e "container=docker" --tmpfs /tmp --tmpfs /run --tmpfs /run/lock -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v $DFM_PATH:/opt/dfm -d dfm
+docker run --privileged=true --cap-add=SYS_ADMIN \
+	-e "container=docker" \
+	--tmpfs /tmp --tmpfs /run --tmpfs /run/lock \
+	-v /sys/fs/cgroup:/sys/fs/cgroup:ro -v $DFM_PATH:/opt/dfm \
+	-p 12345:12345 -p 8080:8080 -p 5600:5600 -p 9200:9200 \
+	--name dfm -d dfm
