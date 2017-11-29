5b50cc802acf30f6ac69488ff66930252e486598
Kalden <kalden_alex@users.sourceforge.net>
2017-11-29 04:59:16 +0100

    ascii encoding is mandatory for deepdetect

--- dfm/feed.py
+++ dfm/feed.py
@@ -578,6 +578,8 @@ class Feed:
             #extract text from the document
             self.logger.debug("Attempting text extraction: "+tmp_file.name)
             text = textract.process(str(tmp_file.name), extension=str(ext))
+            #convert to ascii to be usable by deepdedect
+            text=text.decode().encode('ascii','ignore')
             self.logger.debug("Text Extracted file: "+tmp_file.name+" text size:"+str(len(text)))
             os.unlink(tmp_file.name)
             if len(text)<1:
