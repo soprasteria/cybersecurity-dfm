fdcbf90bef94674bb917cd01af8e4267d45b069a
Kalden <kalden_alex@users.sourceforge.net>
2017-11-29 05:18:35 +0100

    attempt to cleanup the file and fill summary

--- dfm/feed.py
+++ dfm/feed.py
@@ -577,7 +577,17 @@ class Feed:
             last_lib="textract"
             #extract text from the document
             self.logger.debug("Attempting text extraction: "+tmp_file.name)
-            text = textract.process(str(tmp_file.name), extension=str(ext), encoding='ascii')
+            html = textract.process(str(tmp_file.name), extension=str(ext), encoding='ascii')
+            article = Article(url)
+            article.download(html=text)
+            article.parse()
+            html=""
+            article.nlp()
+            text=article.text
+            summary=article.summary
+            title=article.title
+            tags=article.keywords
+            url=article.url
             self.logger.debug("Text Extracted file: "+tmp_file.name+" text size:"+str(len(text)))
             os.unlink(tmp_file.name)
             if len(text)<1:
