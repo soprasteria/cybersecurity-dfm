c304b21ac72eeef071a14138e77124cfb61e43f1
Kalden <kalden_alex@users.sourceforge.net>
2017-11-29 04:49:55 +0100

    fix bug on tmp file removing

--- dfm/feed.py
+++ dfm/feed.py
@@ -579,7 +579,7 @@ class Feed:
             self.logger.debug("Attempting text extraction: "+tmp_file.name)
             text = textract.process(str(tmp_file.name), extension=str(ext))
             self.logger.debug("Text Extracted file: "+tmp_file.name+" text size:"+str(len(text)))
-            os.unlink(tmp_file)
+            os.unlink(tmp_file.name)
             if len(text)<1:
                 results.add_fail({"url":url,"message":"document "+ext+" text extraction failed with textract"})
                 return [doc, results.results]
