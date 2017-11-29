854a8a2180fb549b4dc4a3319824c44c747cacc9
Kalden <kalden_alex@users.sourceforge.net>
2017-11-29 04:47:45 +0100

    improve logging on doc text extraction

--- dfm/feed.py
+++ dfm/feed.py
@@ -578,8 +578,8 @@ class Feed:
             #extract text from the document
             self.logger.debug("Attempting text extraction: "+tmp_file.name)
             text = textract.process(str(tmp_file.name), extension=str(ext))
-            os.unlink(tmp_file)
             self.logger.debug("Text Extracted file: "+tmp_file.name+" text size:"+str(len(text)))
+            os.unlink(tmp_file)
             if len(text)<1:
                 results.add_fail({"url":url,"message":"document "+ext+" text extraction failed with textract"})
                 return [doc, results.results]
