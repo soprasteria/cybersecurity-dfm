909c048155ede0963ed233db0a5889c397edffb0
Kalden <kalden_alex@users.sourceforge.net>
2018-03-11 17:34:03 +0100

    summarise results

--- dfm/server.py
+++ dfm/server.py
@@ -559,6 +559,8 @@ def queueFiller(size, query,work_queue,done_queue, results):
 
     if size is not None:
         query['size']=size
+    else:
+        query['size']=docs=storage.query(query)[0]['hits']['hits']['total']
 
     docs=storage.query(query)[0]['hits']
     results.set_total(int(docs['total']))
