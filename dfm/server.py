3c4ea68e1ab704966aee6b8e0d5ba563b7ca5081
Kalden <kalden_alex@users.sourceforge.net>
2018-03-11 17:37:38 +0100

    fix typo

--- dfm/server.py
+++ dfm/server.py
@@ -560,7 +560,8 @@ def queueFiller(size, query,work_queue,done_queue, results):
     if size is not None:
         query['size']=size
     else:
-        query['size']=int(storage.query(query)[0]['hits']['total'])
+        size=int(storage.query(query)[0]['hits']['total'])
+        query['size']=size
 
     docs=storage.query(query)[0]['hits']
     results.set_total(int(docs['total']))
